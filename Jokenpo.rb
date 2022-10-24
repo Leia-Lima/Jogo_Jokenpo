4f96c72bdab60752fbe25b460aae3ccd82dc6214
Léia <leialimapretty@gmail.com>
2022-10-24 19:13:49 -0300

    Inicio do projeto

---
+++ Jokenpo.rb
@@ -0,0 +1,20 @@
+# Jogo_Jokenpo
+
+opcoes = {
+    1 => 'Pedra',
+    2 => 'Papel',
+    3 => 'Tesoura'
+}
+
+    opcoes.each do |k,v|
+        puts "#{k} - #{v}"
+    end
+
+    print 'Escolha uma opção acima: '
+    sua_escolha = gets.to_i
+
+    
+end
+
+
+
